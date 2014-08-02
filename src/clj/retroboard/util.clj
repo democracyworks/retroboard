bd4b6f04d34eda54bbda82b56e8ad3cf8ad6b95a
Nathan Herzing <nherzing@gmail.com>
2014-08-02 19:33:15 -0400

    Don't use upper and lower case in env id

--- src/clj/retroboard/util.clj
+++ src/clj/retroboard/util.clj
@@ -2,7 +2,6 @@
 
 (def valid-chars
   (map char (concat (range 48 58)
-                    (range 65 91)
                     (range 97 123))))
 
 (defn rand-char []
@@ -10,15 +9,3 @@
 
 (defn rand-str [len]
   (apply str (take len (repeatedly rand-char))))
-
-(defn unique-str [len strs]
-  (-> strs
-      (remove (repeatedly #(rand-str len)))
-      first))
-
-(defn unique-str-generator [len]
-  (let [used-strs (atom #{})]
-    (fn []
-      (let [new-str (unique-str len @used-strs)]
-        (swap! used-strs conj new-str)
-        new-str))))
